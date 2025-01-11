93f0828c3c059b6d8026642c24670391074358e7
Alanas Švažas <asalanys@gmail.com>
2025-01-11 18:37:24 +0200

    CSS and Game.js styling/structure

--- front-end/src/components/Game.js
+++ front-end/src/components/Game.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { useState } from "react";
 
 const Game = () => {
+    const [shipsCount, setShipsCount] = useState(10);
+    const [shotsCount, setShotsCount] = useState(25);
+
     return (
         <div className="game">
-        <p>Game component</p>
+            <div className="game-info">
+                <button>Start New Game</button>
+                <>
+                    <p>Ships remaining: {shipsCount}</p>
+                    <p>Shots remaining: {shotsCount}</p>
+                </>
+            </div>
+            <div className="game-board">
+                <table>
+                    <tbody>
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 }
